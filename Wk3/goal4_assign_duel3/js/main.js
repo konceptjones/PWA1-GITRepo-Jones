261824357bbace6f342196d96def7c4480c25f96
konceptjones <konceptjones@Michaels-MacBook-Pro.local>
2014-12-13 17:30:23 -0500

    editing main.js

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -1,22 +1,29 @@
+//Michael Jones
+//December 13, 2014
+
+/**
+ Assignment 3: Duel 3
+ Part 3/3 of series
+*/
+
 // self-executing function
-//test
 (function fight(){
 
     console.log ("*** Fight  **");
 
-//Define Dom pieces
-var fighter1_txt = document.querySelector("#ww").querySelector("p");
-var fighter2_txt = document.querySelector("#bw").querySelector("p");
-var round_txt = document.querySelector("h5");
-var button = document.getElementById("fight_btn");
+//DOM Pieces Define
+var fighter1_txt = document.querySelector("#ww").querySelector("p"); //fighter one Wonder Woman
+var fighter2_txt = document.querySelector("#bw").querySelector("p"); //fighter two Black Widow
+var round_txt = document.querySelector("h5"); //pulling info from html
+var button = document.getElementById("fight_btn"); //fight button
 
 console.log("ok so far");
 
-//set your click event
+//Event Button
 button.addEventlistener("click", fight, false);
 //DONT USE >> BUTTON.ONCLICK 
 
-//CREATE ARRAY OF OBJECTS FOR 2 FIGHTERS
+//Fighter array documented
 var fighters = [
 {
     name:"Wonder Woman",
@@ -32,10 +39,10 @@ var fighters = [
 
 var round = 1;
 
-//INITIALIZE DOM innerHTML text for top of HTML page
+//INITIALIZE DOM innerHTML 
 round_txt.innerHTML = "Click FIGHT BUTTON to Start!";
-fighter1_txt.innerHTML = fighters[0].name +":  "+ fighters[0].health;
-fighter2_txt.innerHTML = fighters[1].name +":  "+ fighters[1].health;
+fighter1_txt.innerHTML = fighters[0].name +":  "+ fighters[0].health; //declaration of fighter one health
+fighter2_txt.innerHTML = fighters[1].name +":  "+ fighters[1].health; //declaration of fighter two health
 
 //Create "FIGHT" FUNCTION
 function fight(){
@@ -45,8 +52,8 @@ fighter1_txt.innerHTML = fighter[0].name +":    "+fighters[0].health;
 fighter2_txt.innerHTML = fighter[0].name +":    "+fighters[1].health;
 
 //determine damage
-var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage*.5);
-var f2 = Math.floor(Math.random() * fighters[1].damage + fighters[1].damage*.5);
+var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage*.5); // calculation to determine fighter one damage
+var f2 = Math.floor(Math.random() * fighters[1].damage + fighters[1].damage*.5); // calculation to determine fighter two damage
 
 //inflict damage
 fighters[0].health-=f1;
@@ -61,7 +68,6 @@ console.log(result);
 round_txt.innerHTML = "ROUND #" + round + "Results:";
 round++;
 if (results=== "no winner")
-
 {
     fighter1_txt.innerHTML = fighters[0].name +":   " + fighters[0].health;
     fighter2_txt.innerHTML = fighters[1].name +":   " + fighters[1].health;
@@ -71,18 +77,20 @@ if (results=== "no winner")
     fighter2_txt.innerHTML = "";
 }
 
-//disable the button - need the below add
+
 button.removeEventListener("click", fight, false);
+
 
 //button.onclick = "return false";
 document.querySelector('.buttonblue').innerHTML = 'DONE!!!';
 }
+
 }
+
 
 function winnerCheck(){
     var result = "no winner";
     if (fighters[0].health < 1 && fighters[1].health < 1)
-
     {
          result = "You Both Die - GAME OVER!";
     }    else if (fighters[0].health < 1){
